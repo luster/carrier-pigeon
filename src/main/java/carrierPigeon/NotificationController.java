d76a4ed14f4229141dbba38d6c2cdd761a6e72b1
Ethan Lusterman <ethan.lusterman@gmail.com>
2015-01-27 22:56:27 -0500

    quick fix

--- src/main/java/carrierPigeon/NotificationController.java
+++ src/main/java/carrierPigeon/NotificationController.java
@@ -1,5 +1,6 @@
 package carrierPigeon;
 
+import java.util.Arrays;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.PriorityQueue;
@@ -15,6 +16,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RestController
 public class NotificationController {
 
+    // Number of elements to return
+    public final static Integer N = 5;
+
     // Auto-Increment id numbers
     private final AtomicInteger counter = new AtomicInteger(1);
 
@@ -22,18 +26,35 @@ public class NotificationController {
     // by timestamp
     Map<Integer, PriorityQueue<Notification>> db = new HashMap<Integer, PriorityQueue<Notification>>();
 
+    // GET /notifications/by_user/<user_id>
+    // Return most recent N notifications (by timestamp field)
 	@RequestMapping(value="/notifications/by_user/{user_id}", method=RequestMethod.GET)
 	public Notification[] get_notifications(@PathVariable int user_id) {
+
         PriorityQueue<Notification> q = db.get(user_id);
         if (q == null) {
-            q = new PriorityQueue<Notification>(10, new NotificationComparator());
+            q = new PriorityQueue<Notification>(N, new NotificationComparator());
             db.put(user_id, q);
         }
-        Notification[] r = new Notification[q.size()];
-		db.get(user_id).toArray(r);
-        return r;
+
+        // copy PriorityQueue into array and return
+        PriorityQueue<Notification> tmp = new PriorityQueue<Notification>(q);
+        int returnSize = N;
+        if (q.size() < N) {
+            returnSize = q.size();
+        }
+        Notification[] notifications = new Notification[returnSize];
+        int i=0;
+
+        while ( !tmp.isEmpty() && i < N) {
+            notifications[i] = tmp.remove();
+            i++;
+        }
+
+        return notifications;
 	}
 
+    // POST /notifications
     @RequestMapping(value="/notifications",
                     method=RequestMethod.POST,
                     headers={"Content-type=application/json"})
@@ -49,6 +70,8 @@ public class NotificationController {
         }
         q.add(notification);
         db.put(user_id, q);
+
         return notification;
     }
+
 }
